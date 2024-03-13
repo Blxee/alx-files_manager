c42223de5bcc9db10ff4b6ee6ac753ab771eb5e1
Blxee <aminedev57@gmail.com>
2024-03-13 15:55:35 +0000

    Fixing task 1

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
@@ -32,4 +32,4 @@ class DBClient {
 }
 
 const dbClient = new DBClient();
-export default dbClient;
+module.exports = dbClient;
