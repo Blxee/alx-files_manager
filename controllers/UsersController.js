0a5adfef5b19f788004f67d61037301ec728aff8
Blxee <aminedev57@gmail.com>
2024-03-15 12:53:11 +0000

    Fixed: rest of checks for task 3
    The issue was not sending the right error format, it was json not text

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,25 +3,26 @@ const dbClient = require('../utils/db');
 exports.postNew = function postNew(req, res) {
   const user = req.body;
 
+  res.setHeader('Content-Type', 'application/json');
+
   if (!('email' in user)) {
-    res.status(400).end('Missing email');
+    res.status(400).end(JSON.stringify({ error: 'Missing email' }));
     return;
   }
 
   if (!('password' in user)) {
-    res.status(400).end('Missing password');
+    res.status(400).end(JSON.stringify({ error: 'Missing password' }));
     return;
   }
 
   dbClient.userExists(user.email)
     .then((val) => {
       if (val) {
-        res.status(400).end('Already exist');
+        res.status(400).end(JSON.stringify({ error: 'Already exist' }));
       } else {
         dbClient.createUser(user.email, user.password)
           .then((val2) => {
             res.status(201);
-            res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(val2));
           });
       }
